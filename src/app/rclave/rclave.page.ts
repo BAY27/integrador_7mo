173c0bf5c422eac60976bd7f737dc7bece39e061
adavidarevalo <davidarevaloc20@gmail.com>
2024-09-23 10:55:17 -0600

    Added the recover passwor

--- src/app/rclave/rclave.page.ts
+++ src/app/rclave/rclave.page.ts
@@ -12,33 +12,51 @@ export class RclavePage implements OnInit {
   txt_clave: string = '';
   txt_correo: string = '';
   txt_cclave: string = '';
+  codigo: string = '';
   mensaje: string = '';
-coincide:string ='';
+  coincide: string = '';
+
+  // Control de los campos deshabilitados
+  isCodigoDisabled: boolean = true;
+  isClaveDisabled: boolean = true;
+  isCambiarDisabled: boolean = true;
+
   constructor(
     public modalCtrl: ModalController,
     public servicio: AccesoService
   ) {}
 
   ngOnInit() {}
+
   vclave() {
-    if (this.txt_clave == this.txt_cclave) {
-      this.mensaje ='';
-      this.coincide='si';
+    if (this.txt_clave === this.txt_cclave) {
+      this.mensaje = '';
+      this.coincide = 'si';
     } else {
       this.mensaje = 'Las claves no coinciden';
-      this.coincide='no';
+      this.coincide = 'no';
     }
   }
 
+  // Función para habilitar el campo de código
+  enviarCodigo() {
+    this.isCodigoDisabled = false;
+  }
+
+  // Función que se ejecuta cuando se introduce el código
+  checkCodigo() {
+    if (this.codigo.length === 4) {
+      this.isClaveDisabled = false;
+      this.isCambiarDisabled = false;
+    }
+  }
+
+  // Función para cambiar la clave
   cclave() {
-    if (this.coincide =='no') {
+    if (this.coincide === 'no') {
       this.servicio.showToast('Claves no coinciden');
-    } else if (
-      this.txt_cedula == '' ,
-      this.txt_correo == '', 
-      this.txt_cclave == ''
-    ) {
-      this.servicio.showToast('faltan datos');
+    } else if (this.txt_cedula === '' || this.txt_correo === '' || this.txt_cclave === '') {
+      this.servicio.showToast('Faltan datos');
     } else {
       let datos = {
         accion: 'cambiarclave',
@@ -47,7 +65,7 @@ coincide:string ='';
         clave: this.txt_clave,
       };
       this.servicio.postData(datos).subscribe((res: any) => {
-        if (res.estado == true) {
+        if (res.estado === true) {
           this.modalCtrl.dismiss();
           this.servicio.showToast(res.mensaje);
         } else {
